4d81a8d41143563149afd7b955a9473979d34d2a
alfaruqii <bumbblechover@gmail.com>
2023-09-27 07:31:17 +0700

    add: try to add body to a frame

---
+++ snakebody/src/main/java/miku/Body.java
@@ -0,0 +1,14 @@
+package miku;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Body extends JLabel {
+    JLabel snake;
+    Body(){
+        snake = new JLabel();
+        snake.setLayout(null);
+        snake.setOpaque(true);
+        System.out.println("haii");
+    }
+}
--- windowframe/src/main/java/miku/App.java
+++ windowframe/src/main/java/miku/App.java
@@ -1,13 +1,27 @@
 package miku;
 
+import sun.jvm.hotspot.ui.JFrameWrapper;
+
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    static JFrame frame;
+    static Body snake;
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        snake = new Body();
+        frame = new JFrame();
+        snake.setBounds(0,0,50,50);
+        snake.setBackground(Color.RED);
+        frame.add(snake);
+        frame.setLayout(null);
+        frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 }
